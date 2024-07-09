f174c32c029c27824e6a0e071723c1229132a624
soorajabraham1 <soorajabraham1@gmail.com>
2024-07-09 13:51:02 +0200

    added link to companies

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 import random
 
-
+# Load blind75 words
 blind75 = []
 with open('blind75.txt', 'r') as file:
     blind75 = [line.strip() for line in file.readlines()]
 
+# Load questions
 questions = []
 with open('questions.txt', 'r') as file:
     questions = [line.strip() for line in file.readlines()]
 
-companies = []
+# Load companies and their URLs
+companies = {}
 with open('companies.txt', 'r') as file:
-    companies = [line.strip() for line in file.readlines()]
+    companies = {line.split(" - ")[0].strip(): line.split(" - ")[1].strip() for line in file.readlines()}
+
 # Initialize the task list and task index in session state
 if 'task_index' not in st.session_state:
     st.session_state.task_index = 0
@@ -33,16 +36,20 @@ st.write('Click the button to display the next task in order.')
 current_task = st.session_state.tasks[st.session_state.task_index]
 
 if current_task == 'random_word':
-    current_task = random.choice(st.session_state.words) 
-    
-if current_task == 'interview':
-    current_task = random.choice(st.session_state.questions) 
+    current_task = random.choice(st.session_state.words)
+    st.header(current_task)
 
-if current_task == 'companies':
-    current_task = random.sample(st.session_state.companies, 10) 
-    for word in current_task:
-        st.header(word)
-st.header(current_task)
+elif current_task == 'interview':
+    current_task = random.choice(st.session_state.questions)
+    st.header(current_task)
+
+elif current_task == 'companies':
+    current_task = random.sample(list(st.session_state.companies.keys()), 10)
+    for company in current_task:
+        url = st.session_state.companies[company]
+        st.markdown(f"[{company}]({url})", unsafe_allow_html=True)
+else:
+    st.header(current_task)
 
 # Button to go to the next task
 if st.button('Next Task'):
